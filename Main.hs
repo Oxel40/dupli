cd7b482333f528189b8caf2e4f9b50ba91b67cf5
Oxel40 <Oxel40@users.noreply.github.com>
2022-04-18 00:44:16 +0200

    lazy paralell implementation using less ram

--- Main.hs
+++ Main.hs
@@ -1,16 +1,26 @@
 module Main where
 
-import Crypto.Hash.SHA1 (hash)
--- import Data.ByteString (ByteString, readFile)
-import qualified Data.ByteString as BS (ByteString, readFile)
--- import qualified Data.ByteString.Lazy as LBS
+import Crypto.Hash.SHA1 (hashlazy)
+import Control.Concurrent
+import Control.Concurrent.STM
+import Control.Exception (throwIO)
+import Control.Monad (when)
+import Data.ByteString (ByteString)
+import qualified Data.ByteString.Lazy as LBS
 import qualified Data.HashMap as HM
 import System.CPUTime (getCPUTime)
 import System.Directory (doesDirectoryExist, doesFileExist, listDirectory)
 import System.Environment (getArgs, getProgName)
+import System.IO
 import System.Exit (die)
 
-type DupMap = HM.Map BS.ByteString [FilePath]
+nWorkers :: Int
+nWorkers = 4
+
+debug :: Bool
+debug = True
+
+type DupMap = HM.Map ByteString [FilePath]
 
 merge :: [[a]] -> [a]
 merge = merge' []
@@ -32,18 +42,107 @@ main = do
              \Matching files are seperated by a tab character \"\\t\" \
              \and matches are seperated by a newline \"\\n\".\n"
 
+forkThread :: IO () -> IO (MVar ())
+forkThread proc = do
+  handle <- newEmptyMVar
+  _ <- forkFinally proc (\e -> do
+    putMVar handle ()
+    case e of
+      Left ex -> throwIO ex
+      Right _ -> return ()
+    )
+  return handle
+
+forkThreads :: Int -> IO () -> IO [MVar ()]
+forkThreads n proc = mapM (\_ -> forkThread proc) [1..n] 
+
+awaitThreads :: [MVar ()] -> IO ()
+awaitThreads = mapM_ takeMVar
+
+hashWorker :: TChan (Maybe FilePath) -> TChan (ByteString, FilePath) -> IO ()
+hashWorker pathChan hashChan = do
+  tId <- myThreadId
+  when debug $ putStrLn $ show tId ++ ": Started"
+  hw pathChan hashChan
+
+hw :: TChan (Maybe FilePath) -> TChan (ByteString, FilePath) -> IO ()
+hw pathChan hashChan = do
+  -- threadDelay 1000000 -- 1 sec
+  tId <- myThreadId
+  mby <- atomically $ readTChan pathChan
+
+  case mby of
+    Nothing -> when debug $ putStrLn $ show tId ++ ": Nothing in channel"
+    Just path -> do
+      -- when debug $ putStrLn $ show tId ++ ": Just in channel"
+      withFile path ReadMode $ \file -> do
+        -- fileContent <- LBS.readFile path 
+        fileContent <- LBS.hGetContents file
+        let fileHash = hashlazy fileContent 
+        let out = fileHash `seq` (fileHash, path)
+        atomically $ writeTChan hashChan $! out
+      -- when debug $ putStrLn $ show tId ++ ": Wrote to channel"
+      hw pathChan hashChan
+
+  -- threadDelay 1000000 -- 1 sec
+      -- hashWorker pathChan hashChan
+-- hashes <- mapM (\p -> LBS.readFile p >>= (\bs -> return (hashlazy bs, p))) $ merge entrys
+
 findDuplicates :: [FilePath] -> IO ()
 findDuplicates paths = do
   time0 <- getCPUTime
   entrys <- mapM reqList paths
+  ----------
   time1 <- getCPUTime
-  -- mapM_ putStrLn entrys
-  hashes <- mapM (\p -> BS.readFile p >>= (\bs -> return (hash bs, p))) $ merge entrys
+
+  pathChan <- newTChanIO
+  hashChan <- newTChanIO
+
+  let worker = hashWorker pathChan hashChan
+
+  -- start threads
+  threads <- forkThreads nWorkers worker
+  -- feed workers
+  print $ length $ merge entrys
+  -- let allPaths = take 1014 $ merge entrys
+  let allPaths = merge entrys
+  threadDelay 3000000
+  mapM_ (atomically . writeTChan pathChan . Just) allPaths
+  mapM_ (atomically . writeTChan pathChan . const Nothing) [1..(nWorkers*2)]
+  threadDelay 3000000
+  -- await threads
+  when debug $ putStrLn "Awaiting threads..."
+  awaitThreads threads
+  when debug $ putStrLn "Awaiting threads... Done"
+  -- hashes <- getChanContents hashChan
+  when debug $ putStrLn "Reading from chan..."
+  -- _ <- atomically $ readTChan hashChan 
+  {-
+  i <- newMVar 1 :: IO (MVar Int)
+  _ <- forever $ do
+    im <- takeMVar i
+    mb <- atomically $ tryReadTChan hashChan
+    case mb of
+      Nothing -> do
+        when debug $ putStrLn $ show im ++ " Nothing in channel"
+        _ <- atomically $ readTChan hashChan
+        return ()
+      Just _ -> return ()
+    -- when debug $ putStrLn $ show im ++ " Single read from chan done"
+    putMVar i (im+1)
+  -}
+
+  hashes <- mapM (\_ -> atomically $ readTChan hashChan) allPaths
+  when debug $ putStrLn "Reading from chan... Done"
+
+  ----------
   time2 <- getCPUTime
-  -- mapM_ (\(h, p) -> putStr (Char8.unpack $ B.toLazyByteString $ B.byteStringHex h) >> putStr " : " >> putStrLn p) hashes
   let doups = filterDuplicates hashes
+  ----------
   time3 <- getCPUTime
   mapM_ (\(_, ps) -> mapM_ (\p -> putStr p >> putStr "\t") ps >> putStr "\n") $ HM.toList doups
+
+  when debug $ print $ length hashes
 
   putStr "Finding entries: "
   print $ (time1 - time0)*100 `div` (time3 - time0)
@@ -52,10 +151,10 @@ findDuplicates paths = do
   putStr "Filtering doup hashes: "
   print $ (time3 - time2)*100 `div` (time3 - time0)
 
-filterDuplicates :: [(BS.ByteString, FilePath)] -> DupMap
+filterDuplicates :: [(ByteString, FilePath)] -> DupMap
 filterDuplicates = fd HM.empty
 
-fd :: DupMap -> [(BS.ByteString, FilePath)] -> DupMap
+fd :: DupMap -> [(ByteString, FilePath)] -> DupMap
 fd hMap [] = HM.filter (\ps -> length ps > 1) hMap
 fd hMap ((bs, p) : rest)
   | HM.member bs hMap = fd hMap' rest
